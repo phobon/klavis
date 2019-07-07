7fbd22ea22268b7b063f0b2781a5c99476cd5314
phobon <exponen7@gmail.com>
2019-07-07 18:40:15 +0800

    Removing console log

--- src/components/Tooltip/withTooltip.js
+++ src/components/Tooltip/withTooltip.js
@@ -14,7 +14,6 @@ const withTooltip = WrappedComponent => ({ tooltip, tooltipDirection, offset, as
   }
 
   const [{ fullWidth, fullHeight, width, height, position, ...layoutProps}, passthroughProps] = destructureLayoutProps(props);
-  console.log(destructureLayoutProps(props));
   return (
     <Tooltip
       tooltip={tooltip}
