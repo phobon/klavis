36228f0360a970406078db05df9732208cb64dac
phobon <exponen7@gmail.com>
2020-04-18 12:56:23 +0800

    Updating button API

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,230 +1,187 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { cover, focus, gridPosition } from '@phobon/base';
-import { compose, space, layout, border, flexbox, typography, position } from 'styled-system';
+import { variant, compose, space, layout, border, flexbox, typography, position } from 'styled-system';
 import propTypes from '@styled-system/prop-types';
 import PropTypes from 'prop-types';
 import shouldForwardProp from '@styled-system/should-forward-prop';
 
 import withTooltip from '../Tooltip';
 
-const buttonVariant = props => {
-  const tertiary = css`
-    background-color: transparent;
-    border-color: transparent;
-    color: ${props.theme.colors.grayscale[1]};
-    fill: ${props.theme.colors.grayscale[1]};
-    &:hover {
-      background-color: ${props.theme.colors.grayscale[7]};
-    }
-  `;
-  const buttonVariants = {
-    primary: css`
-      background-color: ${props.theme.colors.accent[4]};
-      border-color: ${props.theme.colors.accent[4]};
-      color: white;
-      fill: white;
-      &:hover {
-        background-color: ${props.theme.colors.accent[2]};
-        color: white;
-        fill: white;
-      }
-    `,
-    secondary: css`
-      background-color: ${props.theme.colors.grayscale[8]};
-      border-color: ${props.theme.colors.grayscale[7]};
-      color: ${props.theme.colors.grayscale[1]};
-      fill: ${props.theme.colors.grayscale[1]};
-      &:hover {
-        background-color: ${props.theme.colors.grayscale[7]};
-      }
-    `,
-    tertiary,
-
-    warning: css`
-      background-color: ${props.theme.colors.guidance.warning[1]};
-      border-color: ${props.theme.colors.guidance.warning[0]};
-      color: ${props.theme.colors.guidance.warning[0]};
-      fill: ${props.theme.colors.guidance.warning[0]};
-      &:hover {
-        background-color: ${props.theme.colors.oranges[7]};
-        color: ${props.theme.colors.guidance.warning[0]};
-        fill: ${props.theme.colors.guidance.warning[0]};
-      }
-    `,
-    danger: css`
-      background-color: ${props.theme.colors.guidance.error[1]};
-      border-color: ${props.theme.colors.guidance.error[0]};
-      color: ${props.theme.colors.guidance.error[0]};
-      fill: ${props.theme.colors.guidance.error[0]};
-      &:hover {
-        background-color: ${props.theme.colors.reds[7]};
-        color: ${props.theme.colors.guidance.error[0]};
-        fill: ${props.theme.colors.guidance.error[0]};
-      }
-    `,
-    success: css`
-      background-color: ${props.theme.colors.guidance.success[1]};
-      border-color: ${props.theme.colors.guidance.success[0]};
-      color: ${props.theme.colors.guidance.success[0]};
-      fill: ${props.theme.colors.guidance.success[0]};
-      &:hover {
-        background-color: ${props.theme.colors.greens[7]};
-        color: ${props.theme.colors.guidance.success[0]};
-        fill: ${props.theme.colors.guidance.success[0]};
-      }
-    `,
-
-    link: css`
-      background-color: transparent;
-      border: 0;
-      color: ${props.theme.colors.grayscale[1]};
-      fill: ${props.theme.colors.grayscale[1]};
-      padding: 0;
-      text-decoration: underline dotted;
-      &:hover {
-        color: ${props.theme.colors.accent[2]};
-        fill: ${props.theme.colors.accent[2]};
-        text-decoration: underline;
-      }
-    `,
-    clean: css`
-      background: 0;
-      border: 0;
-      color: inherit;
-      fill: inherit;
-      padding: 0;
-      width: unset;
-      height: unset;
-      &:hover {
-        color: inherit;
-        fill: inherit;
-      }
-    `,
-  };
-
-  return buttonVariants[props.variant];
+const variants = {
+  primary: {
+    color: 'white',
+    fill: 'white',
+    bg: 'accent.4',
+    '&:hover': {
+      bg: 'accent.2',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'accent.2',
+      '&:hover': {
+        bg: 'accent.4',
+      },
+    },
+  },
+  secondary: {
+    color: 'grayscale.1',
+    fill: 'grayscale.1',
+    bg: 'grayscale.8',
+    '&:hover': {
+      bg: 'grayscale.7',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'grayscale.2',
+      color: 'grayscale.9',
+      '&:hover': {
+        bg: 'grayscale.4',
+      },
+    },
+  },
+  tertiary: {
+    color: 'grayscale.1',
+    fill: 'grayscale.1',
+    bg: 'transparent',
+    '&:hover': {
+      bg: 'grayscale.7',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'grayscale.2',
+      color: 'grayscale.9',
+      '&:hover': {
+        bg: 'grayscale.4',
+      },
+    },
+  },
+  warning: {
+    bg: 'guidance.warning.1',
+    color: 'guidance.warning.0',
+    fill: 'guidance.warning.0',
+    '&:hover': {
+      bg: 'oranges.7',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'oranges.6',
+      '&:hover': {
+        bg: 'oranges.7',
+      },
+    },
+  },
+  danger: {
+    bg: 'guidance.error.1',
+    color: 'guidance.error.0',
+    fill: 'guidance.error.0',
+    '&:hover': {
+      bg: 'reds.7',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'reds.6',
+      '&:hover': {
+        bg: 'reds.7',
+      },
+    },
+  },
+  success: {
+    bg: 'guidance.success.1',
+    color: 'guidance.success.0',
+    fill: 'guidance.success.0',
+    '&:hover': {
+      bg: 'greens.7',
+    },
+    '&[aria-pressed="true"]': {
+      bg: 'greens.6',
+      '&:hover': {
+        bg: 'greens.7',
+      },
+    },
+  },
+  link: {
+    bg: 'transparent',
+    border: 'none',
+    color: 'grayscale.1',
+    fill: 'grayscale.1',
+    padding: 0,
+    textDecoration: 'underline dotted',
+    '&:hover': {
+      color: 'accent.2',
+      fill: 'accent.2',
+      textDecoration: 'underline',
+    },
+    '&[aria-pressed="true"]': {
+      color: 'accent.4',
+      fill: 'accent.4',
+      textDecoration: 'underline',
+      '&:hover': {
+        color: 'grayscale.1',
+        fill: 'grayscale.1',
+        textDecoration: 'underline dotted',
+      },
+    },
+  },
+  clean: {
+    bg: 'transparent',
+    border: 0,
+    color: 'inherit',
+    fill: 'inherit',
+    padding: 0,
+    width: 'unset',
+    height: 'unset',
+  },
 };
 
-const pressed = props => {
-  const tertiary = css`
-    background-color: ${props.theme.colors.grayscale[2]};
-    color: ${props.theme.colors.grayscale[9]};
-    &:hover {
-      background-color: ${props.theme.colors.grayscale[4]};
-    }
-  `;
-  const toggledVariants = {
-    primary: css`
-      background-color: ${props.theme.colors.accent[2]};
-      &:hover {
-        background-color: ${props.theme.colors.accent[4]};
-      }
-    `,
-    secondary: css`
-      background-color: ${props.theme.colors.grayscale[2]};
-      color: ${props.theme.colors.grayscale[9]};
-      &:hover {
-        background-color: ${props.theme.colors.grayscale[4]};
-      }
-    `,
-    tertiary,
-
-    warning: css`
-      background-color: ${props.theme.colors.oranges[6]};
-      &:hover {
-        background-color: ${props.theme.colors.oranges[7]};
-      }
-    `,
-    danger: css`
-      background-color: ${props.theme.colors.reds[6]};
-      &:hover {
-        background-color: ${props.theme.colors.reds[7]};
-      }
-    `,
-    success: css`
-      background-color: ${props.theme.colors.greens[6]};
-      &:hover {
-        background-color: ${props.theme.colors.greens[7]};
-      }
-    `,
-
-    link: css`
-      color: ${props.theme.colors.accent[4]};
-      fill: ${props.theme.colors.accent[4]};
-      text-decoration: underline;
-      &:hover {
-        color: ${props.theme.colors.grayscale[1]};
-        fill: ${props.theme.colors.grayscale[1]};
-        text-decoration: underline dotted;
-      }
-    `,
-    clean: css`
-    `,
-  };
-
-  return toggledVariants[props.variant];
-};
-
-const size = props => {
+const size = ({ theme, pl, pr, size: propsSize }) => {
   const sizes = {
-    s: css`
-      min-width: ${props.theme.space[4]}px;
-      height: ${props.theme.space[4]}px;
-      padding-left: ${props.theme.space[props.pl || 2]}px;
-      padding-right: ${props.theme.space[props.pr || 2]}px;
-    `,
-    m: css`
-      min-width: ${props.theme.space[5]}px;
-      height: ${props.theme.space[5]}px;
-      padding-left: ${props.theme.space[props.pl || 3]}px;
-      padding-right: ${props.theme.space[props.pr || 3]}px;
-    `,
-    l: css`
-      min-width: ${props.theme.space[6]}px;
-      height: ${props.theme.space[6]}px;
-      padding-left: ${props.theme.space[props.pl || 3]}px;
-      padding-right: ${props.theme.space[props.pr || 3]}px;
-    `,
+    s: {
+      minWidth: theme.space[4],
+      height: theme.space[4],
+      paddingLeft: theme.space[pl || 2],
+      paddingRight: theme.space[pr || 2],
+    },
+    m: {
+      minWidth: theme.space[5],
+      height: theme.space[5],
+      paddingLeft: theme.space[pl || 3],
+      paddingRight: theme.space[pr || 3],
+    },
+    l: {
+      minWidth: theme.space[6],
+      height: theme.space[6],
+      paddingLeft: theme.space[pl || 3],
+      paddingRight: theme.space[pr || 3],
+    },
   };
 
-  return sizes[props.size];
+  return sizes[propsSize];
 };
 
-const buttonStyles = compose(space, layout, border, flexbox, typography, position);
+const disabled = ({ theme }) => ({
+  '&:disabled': {
+    opacity: 0.5,
+    backgroundColor: theme.colors.grayscale[7],
+    border: 0,
+    color: theme.colors.grayscale[4],
+    fill: theme.colors.grayscale[4],
+    pointerEvents: 'none',
+  },
+});
 
-const Button = styled('button').withConfig({
-  shouldForwardProp,
-}).attrs(props => ({
+const buttonStyles = compose(space, layout, border, flexbox, typography, position, cover);
+
+const Button = styled('button').attrs(props => ({
   'aria-pressed': props.toggled ? 'true' : undefined,
-}))`
-  box-sizing: border-box;
-  position: relative;
-
-  cursor: pointer;
-
-  transition: all 120ms ease-out;
-
-  ${buttonStyles}
-
-  ${cover}
-  ${size}
-  ${buttonVariant}
-
-  &[aria-pressed="true"] {
-    ${pressed}
-  }
-
-  ${focus}
-
-  &:disabled {
-    opacity: 0.5;
-    background-color: ${props => props.theme.colors.grayscale[6]};
-    border: 0;
-    color: ${props => props.theme.colors.grayscale[4]};
-    fill: ${props => props.theme.colors.grayscale[4]};
-    pointer-events: none;
-  }
-`;
+})).withConfig({
+  shouldForwardProp,
+})({
+    boxSizing: 'border-box',
+    position: 'relative',
+    cursor: 'pointer',
+    transition: 'all 120ms ease-out',
+  },
+  buttonStyles,
+  size,
+  focus,
+  variant({ variants }),
+  disabled,
+);
 
 Button.displayName = 'Button';
 
