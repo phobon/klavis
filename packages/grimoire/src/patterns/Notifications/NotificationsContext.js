ed9227440c2779b12fd6f84d18013f95140de927
Ben McCormick <exponen7@gmail.com>
2020-10-08 10:10:43 +0800

    Convert Notifications

--- packages/grimoire/src/patterns/Notifications/NotificationsContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NotificationsContext = React.createContext();
-
-export default NotificationsContext;
---
+++ packages/grimoire/src/patterns/Notifications/useNotifications.tsx
@@ -0,0 +1,5 @@
+import { useContext } from 'react';
+
+import { NotificationsContext } from './NotificationsContext';
+
+export const useNotifications = () => useContext(NotificationsContext);
